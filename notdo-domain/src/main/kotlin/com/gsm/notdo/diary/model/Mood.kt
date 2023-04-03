b02ff1a519b56d8d0ab4e14e7ebbd7a6d84c50e0
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-04-03 16:12:16 +0900

    :sparkles: mood enum 생성

---
+++ notdo-domain/src/main/kotlin/com/gsm/notdo/diary/model/Mood.kt
@@ -0,0 +1,11 @@
+package com.gsm.notdo.diary.model
+
+enum class Mood (
+    val mood: String
+) {
+    NORMAL("기본 표정"),
+    ANGRY("화난 표정"),
+    SAD("슬픈 표정"),
+    BAD("별로인 얼굴"),
+    GOOD("좋은 얼굴")
+}
