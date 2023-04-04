53fd62951592bad7d1efaaffd59b24b26675cc31
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-04-04 16:04:38 +0900

    :recycle: user persistence entity로 변경

--- notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/user/entity/UserEntity.kt
+++ notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/user/entity/UserEntity.kt
@@ -2,6 +2,7 @@ package com.gsm.notdo.persistence.user.entity
 
 import java.time.ZonedDateTime
 import java.util.UUID
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
 
@@ -9,8 +10,11 @@ import javax.persistence.Id
 data class UserEntity(
         @Id
         val id: UUID,
+        @Column(columnDefinition = "VARCHAR(255)", nullable = false, unique = true)
         val email: String,
+        @Column(nullable = true)
         val password: String?,
+        @Column(columnDefinition = "VARCHAR(20)", nullable = false)
         val nickName: String,
         val createdAt: ZonedDateTime
 )
