74d6132b8df46c16efecf56b00fb5a8fb3257493
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-04-04 18:14:06 +0900

    :recycle: diaray persistence entity 수정

--- notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/diary/entity/DiaryEntity.kt
+++ notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/diary/entity/DiaryEntity.kt
@@ -3,18 +3,21 @@ package com.gsm.notdo.persistence.diary.entity
 import com.gsm.notdo.domain.diary.model.Mood
 import com.gsm.notdo.persistence.user.entity.UserEntity
 import java.time.LocalDateTime
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.ManyToOne
+import javax.persistence.*
 
 @Entity
 data class DiaryEntity(
         @Id
         val id: Long,
+        @Column(columnDefinition = "VARCHAR2(100)", nullable = false)
         val title: String,
+        @Column(columnDefinition = "VARCHAR2(1000)", nullable = false)
         val content: String,
+        @Enumerated(EnumType.STRING)
+        @Column(columnDefinition = "VARCHAR2(7)", nullable = false)
         val mood: Mood,
         val createdAt: LocalDateTime,
-        @ManyToOne
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)", nullable = false)
         val userId: UserEntity
 )
