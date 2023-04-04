014eac0e2d696993627fccf30f3e297d0cf75b42
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-04-04 15:58:09 +0900

    :recycle: db 옵션 추가

--- notdo-domain/src/main/kotlin/com/gsm/notdo/domain/user/model/User.kt
+++ notdo-domain/src/main/kotlin/com/gsm/notdo/domain/user/model/User.kt
@@ -3,12 +3,18 @@ package com.gsm.notdo.domain.user.model
 import com.gsm.notdo.common.annotation.Aggregate
 import java.time.ZonedDateTime
 import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Id
 
 @Aggregate
 data class User(
+        @Id
         val id: UUID,
+        @Column(columnDefinition = "VARCHAR(255)", nullable = false, unique = true)
         val email: String,
+        @Column(columnDefinition = "VARCHAR(255)", nullable = true)
         val password: String?,
+        @Column(columnDefinition = "VARCHAR(20)", nullable = false)
         val nickName: String,
         val createdAt: ZonedDateTime
 )
