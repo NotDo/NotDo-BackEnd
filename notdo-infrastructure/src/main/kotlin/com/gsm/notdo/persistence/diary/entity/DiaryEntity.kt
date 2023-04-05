c50390053d552d4b03139f18bbe2c22ca5dcd5fb
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-04-05 09:52:10 +0900

    :recycle: base entity 적용

--- notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/diary/entity/DiaryEntity.kt
+++ notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/diary/entity/DiaryEntity.kt
@@ -1,14 +1,15 @@
 package com.gsm.notdo.persistence.diary.entity
 
 import com.gsm.notdo.domain.diary.model.Mood
+import com.gsm.notdo.persistence.common.BaseEntity
 import com.gsm.notdo.persistence.user.entity.UserEntity
 import java.time.LocalDateTime
+import java.util.UUID
 import javax.persistence.*
 
 @Entity
 data class DiaryEntity(
-        @Id
-        val id: Long,
+        override val id: UUID,
         @Column(columnDefinition = "VARCHAR2(100)", nullable = false)
         val title: String,
         @Column(columnDefinition = "VARCHAR2(1000)", nullable = false)
@@ -16,8 +17,7 @@ data class DiaryEntity(
         @Enumerated(EnumType.STRING)
         @Column(columnDefinition = "VARCHAR2(7)", nullable = false)
         val mood: Mood,
-        val createdAt: LocalDateTime,
         @ManyToOne(fetch = FetchType.LAZY)
         @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)", nullable = false)
         val userId: UserEntity
-)
+) : BaseEntity(id)
--- notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/user/entity/UserEntity.kt
+++ notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/user/entity/UserEntity.kt
@@ -1,5 +1,7 @@
 package com.gsm.notdo.persistence.user.entity
 
+import com.gsm.notdo.persistence.common.BaseEntity
+import java.time.LocalDateTime
 import java.time.ZonedDateTime
 import java.util.UUID
 import javax.persistence.Column
@@ -8,13 +10,11 @@ import javax.persistence.Id
 
 @Entity
 data class UserEntity(
-        @Id
-        val id: UUID,
+        override val id: UUID,
         @Column(columnDefinition = "VARCHAR2(255)", nullable = false, unique = true)
         val email: String,
         @Column(columnDefinition = "VARCHAR2(255)" ,nullable = true)
         val password: String?,
         @Column(columnDefinition = "VARCHAR2(8)", nullable = false)
-        val nickName: String,
-        val createdAt: ZonedDateTime
-)
+        val nickName: String
+) : BaseEntity(id)
