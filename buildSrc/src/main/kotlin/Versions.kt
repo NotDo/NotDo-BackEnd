7c380c1295c15d19734dcf57cef1c301663541f6
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-04-06 20:51:30 +0900

    :sparkles: dependancy object 추가

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,7 @@
+object Versions {
+    const val QUERYDSL = "5.0.0"
+    const val MOCKK = "1.12.0"
+    const val KOTEST_JUNIT = "5.3.2"
+    const val KOTEST_EXTENSION = "1.1.2"
+    const val KOTEST_ASSERTIONS = "5.3.2"
+}
