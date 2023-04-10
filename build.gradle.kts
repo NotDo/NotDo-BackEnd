14d65add48501fd3250fc4441744ec27ba99396f
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-04-11 07:36:42 +0900

    :heavy_plus_sign: dependency들 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,13 @@ allprojects {
         test {
             useJUnitPlatform()
         }
+        dependencies {
+            testImplementation("org.springframework.boot:spring-boot-starter-test")
+            testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
+            testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+            testImplementation("io.kotest:kotest-assertions-core:5.5.5")
+            testImplementation("io.mockk:mockk:1.13.4")
+        }
     }
     repositories {
         mavenCentral()
--- notdo-infrastructure/build.gradle.kts
+++ notdo-infrastructure/build.gradle.kts
@@ -14,11 +14,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation(Dependencies.Web.SPRING_WEB)
     runtimeOnly("com.mysql:mysql-connector-j")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
-    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
-    testImplementation("io.kotest:kotest-assertions-core:5.5.5")
-    testImplementation("io.mockk:mockk:1.13.4")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
