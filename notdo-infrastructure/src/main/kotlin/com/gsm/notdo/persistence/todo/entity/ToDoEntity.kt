2e17cee18a1073bcd61c7c09500e13888b14054d
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-04-16 17:07:21 +0900

    :sparkles: todo repository 생성

--- notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/todo/entity/ToDoEntity.kt
+++ notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/todo/entity/ToDoEntity.kt
@@ -1,15 +1,13 @@
 package com.gsm.notdo.persistence.todo.entity
 
-import com.gsm.notdo.common.annotation.Aggregate
 import com.gsm.notdo.persistence.common.BaseEntity
 import com.gsm.notdo.persistence.user.entity.UserEntity
-import org.hibernate.annotations.ColumnDefault
 import java.util.UUID
 import javax.persistence.*
 
 @Entity
 @Table(name = "tbl_todo")
-data class ToDo(
+data class ToDoEntity(
         override val id: UUID,
         @Column(columnDefinition = "VARCHAR(50)", nullable = false)
         val content: String,
