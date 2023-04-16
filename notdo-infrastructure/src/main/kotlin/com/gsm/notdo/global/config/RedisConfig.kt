c96197d5e2bb7af784bcc45c8541d8f28e2a0c85
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-04-17 00:04:07 +0900

    :sparkles: config 생성

---
+++ notdo-infrastructure/src/main/kotlin/com/gsm/notdo/global/config/RedisConfig.kt
@@ -0,0 +1,38 @@
+package com.gsm.notdo.global.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.ZSetOperations
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializer
+
+@Configuration
+class RedisConfig(
+    @Value("\${spring.redis.host}")
+    private val redisHost: String,
+    @Value("\${spring.redis.port}")
+    private val redisPort: Int
+) {
+    @Bean
+    @ConditionalOnMissingBean(RedisConnectionFactory::class)
+    fun redisConnectionFactory(): RedisConnectionFactory =
+            LettuceConnectionFactory(redisHost, redisPort)
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any> {
+        val redisTemplate: RedisTemplate<String, Any> = RedisTemplate()
+        redisTemplate.setConnectionFactory(redisConnectionFactory())
+        redisTemplate.keySerializer = RedisSerializer.string()
+        redisTemplate.valueSerializer = GenericJackson2JsonRedisSerializer()
+
+        return redisTemplate
+    }
+
+    @Bean
+    fun zSetOperation(): ZSetOperations<String, Any> = redisTemplate().opsForZSet()
+}
