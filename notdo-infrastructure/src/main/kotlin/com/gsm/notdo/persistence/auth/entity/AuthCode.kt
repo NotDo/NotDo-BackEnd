68002acdb2b1afae7315a8738d02c579dfed9cac
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-04-17 08:18:56 +0900

    :recycle: 어노테이션 변경

--- notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/auth/entity/AuthCode.kt
+++ notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/auth/entity/AuthCode.kt
@@ -1,11 +1,11 @@
 package com.gsm.notdo.persistence.auth.entity
 
-import com.gsm.notdo.common.annotation.Aggregate
+import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.TimeToLive
 import org.springframework.data.redis.core.index.Indexed
 import javax.persistence.Id
 
-@Aggregate
+@RedisHash
 data class AuthCode(
         @Id
         val email: String,
