a83090ae00bee1e4667d3cf6fd0e1b7da3cedc91
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-04-17 20:06:45 +0900

    :sparkles: filter 생성

--- notdo-application/src/main/kotlin/com/gsm/notdo/common/error/BasicException.kt
+++ notdo-application/src/main/kotlin/com/gsm/notdo/common/error/BasicException.kt
@@ -1,6 +1,6 @@
 package com.gsm.notdo.common.error
 
-abstract class BaseException(
+abstract class BasicException(
         val errorProperty: ErrorProperty
 ) : RuntimeException() {
     override fun fillInStackTrace(): Throwable = this
