294e0f3fef899d7791b368b345fd80d63da227c0
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-04-20 12:18:00 +0900

    :recycle: 파일 위치 수정

--- notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/auth/entity/RefreshTokenEntity.kt
+++ notdo-infrastructure/src/main/kotlin/com/gsm/notdo/persistence/auth/entity/RefreshTokenEntity.kt
@@ -1,9 +1,9 @@
 package com.gsm.notdo.persistence.auth.entity
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.TimeToLive
 import org.springframework.data.redis.core.index.Indexed
-import javax.persistence.Id
 
 @RedisHash
 data class RefreshTokenEntity(
