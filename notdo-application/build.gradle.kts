d2ee2917fae48407de0afae1e3741e8151282895
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-04-27 20:18:13 +0900

    :sparkles: jwt port 생성

--- notdo-application/build.gradle.kts
+++ notdo-application/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
+    implementation(project(":notdo-presentation"))
     implementation(project(":notdo-domain"))
     implementation(Dependencies.Transaction.SPRING_TRANSACTION)
 }
--- notdo-application/src/main/kotlin/com/gsm/notdo/domain/auth/port/output/JwtPort.kt
+++ notdo-application/src/main/kotlin/com/gsm/notdo/domain/auth/port/output/JwtPort.kt
@@ -1,7 +1,8 @@
 package com.gsm.notdo.domain.auth.port.output
 
+import com.gsm.notdo.domain.user.input.response.TokenResponse
 import java.util.UUID
 
 interface JwtPort {
-    fun receiveToken(userId: UUID)
+    fun receiveToken(userId: UUID): TokenResponse
 }
