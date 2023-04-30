c6f579229f4fadba99ddfc3810533dd0095db196
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-05-01 07:57:18 +0900

    :recycle: 어노테이션 추가

--- notdo-infrastructure/src/main/kotlin/com/gsm/notdo/global/security/config/SecurityConfig.kt
+++ notdo-infrastructure/src/main/kotlin/com/gsm/notdo/global/security/config/SecurityConfig.kt
@@ -5,10 +5,12 @@ import com.gsm.notdo.global.security.token.JwtParser
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig(
         private val jwtParser: JwtParser,
         private val objectMapper: ObjectMapper
