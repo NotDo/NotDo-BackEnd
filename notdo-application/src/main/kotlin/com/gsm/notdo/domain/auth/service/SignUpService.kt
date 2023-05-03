c047308204ba44be52552d5471fb6d9b89963ffd
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-05-03 11:24:35 +0900

    :sparkles: 서비스 구현

--- notdo-application/src/main/kotlin/com/gsm/notdo/domain/auth/service/SignUpService.kt
+++ notdo-application/src/main/kotlin/com/gsm/notdo/domain/auth/service/SignUpService.kt
@@ -5,8 +5,12 @@ import com.gsm.notdo.domain.auth.port.input.SignUpUseCase
 import com.gsm.notdo.domain.auth.port.input.dto.SignUpDto
 import com.gsm.notdo.domain.auth.port.output.JwtPort
 import com.gsm.notdo.domain.auth.port.output.dto.TokenDto
+import com.gsm.notdo.domain.user.exception.UserAlreadyExistException
+import com.gsm.notdo.domain.user.model.User
 import com.gsm.notdo.domain.user.port.output.CommandUserPort
 import com.gsm.notdo.domain.user.port.output.QueryUserPort
+import java.time.LocalDate
+import java.util.*
 
 @Service
 class SignUpService(
@@ -15,5 +19,17 @@ class SignUpService(
         private val jwtPort: JwtPort
 ) : SignUpUseCase {
     override fun execute(dto: SignUpDto): TokenDto {
+        if(queryUserPort.existUserByEmail(dto.email)) {
+            throw UserAlreadyExistException()
+        }
+        val user = User(
+                id = UUID.randomUUID(),
+                email = dto.email,
+                password = dto.password,
+                nickname = dto.nickname,
+                createdAt = LocalDate.now()
+        )
+        commandUserPort.create(user)
+        return jwtPort.receiveToken(user.id)
     }
 }
