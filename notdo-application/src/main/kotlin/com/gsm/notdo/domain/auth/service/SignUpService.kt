36eb4b663dd2564946445072390f7550b3371700
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-05-03 12:05:50 +0900

    :recycle: password encoder 적용

--- notdo-application/src/main/kotlin/com/gsm/notdo/domain/auth/service/SignUpService.kt
+++ notdo-application/src/main/kotlin/com/gsm/notdo/domain/auth/service/SignUpService.kt
@@ -4,6 +4,7 @@ import com.gsm.notdo.common.annotation.Service
 import com.gsm.notdo.domain.auth.port.input.SignUpUseCase
 import com.gsm.notdo.domain.auth.port.input.dto.SignUpDto
 import com.gsm.notdo.domain.auth.port.output.JwtPort
+import com.gsm.notdo.domain.auth.port.output.PasswordEncodePort
 import com.gsm.notdo.domain.auth.port.output.dto.TokenDto
 import com.gsm.notdo.domain.user.exception.UserAlreadyExistException
 import com.gsm.notdo.domain.user.model.User
@@ -16,6 +17,7 @@ import java.util.*
 class SignUpService(
         private val queryUserPort: QueryUserPort,
         private val commandUserPort: CommandUserPort,
+        private val passwordEncoderPort: PasswordEncodePort,
         private val jwtPort: JwtPort
 ) : SignUpUseCase {
     override fun execute(dto: SignUpDto): TokenDto {
@@ -25,7 +27,7 @@ class SignUpService(
         val user = User(
                 id = UUID.randomUUID(),
                 email = dto.email,
-                password = dto.password,
+                password = passwordEncoderPort.encode(dto.password),
                 nickname = dto.nickname,
                 createdAt = LocalDate.now()
         )
