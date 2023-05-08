d79de651fcb083e027728307239bac1bb757fd33
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-05-08 10:51:53 +0900

    :recycle: Save 반환값 추가

--- notdo-presentation/src/main/kotlin/com/gsm/notdo/domain/user/output/UserCommandPersistenceAdapter.kt
+++ notdo-presentation/src/main/kotlin/com/gsm/notdo/domain/user/output/UserCommandPersistenceAdapter.kt
@@ -1,6 +1,7 @@
 package com.gsm.notdo.domain.user.output
 
 import com.gsm.notdo.domain.user.model.User
+import com.gsm.notdo.domain.user.output.mapper.toDomain
 import com.gsm.notdo.domain.user.output.mapper.toEntity
 import com.gsm.notdo.domain.user.output.persistence.repository.UserRepository
 import com.gsm.notdo.domain.user.port.output.CommandUserPort
@@ -11,7 +12,8 @@ class UserCommandPersistenceAdapter(
     private val userRepository: UserRepository
 ) : CommandUserPort {
     override fun create(domain: User): User {
-       val userEntity = domain.toEntity(domain)
-        userRepository.save(userEntity)
+        val userEntity = domain.toEntity(domain)
+        val saveEntity = userRepository.save(userEntity)
+        return saveEntity.toDomain(saveEntity)
     }
 }
