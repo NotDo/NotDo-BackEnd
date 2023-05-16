de50e38935a21b5699d00b5ace31f414fd9d5b7b
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-05-16 14:54:08 +0900

    :sparkles: Usecase 생성

---
+++ src/main/kotlin/com/gsm/notdo/domain/auth/application/port/input/SignInUseCase.kt
@@ -0,0 +1,8 @@
+package com.gsm.notdo.domain.auth.application.port.input
+
+import com.gsm.notdo.domain.auth.application.port.input.dto.SignInDto
+import com.gsm.notdo.domain.auth.port.output.dto.TokenDto
+
+interface SignInUseCase {
+    fun execute(signInDto: SignInDto): TokenDto
+}
