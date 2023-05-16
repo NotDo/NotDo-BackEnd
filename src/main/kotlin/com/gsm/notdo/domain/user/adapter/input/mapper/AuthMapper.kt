4c8d78e0beacaf3ed9884a2fc841653c0f79b655
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-05-16 15:05:51 +0900

    :sparkles: Controller 구현

--- src/main/kotlin/com/gsm/notdo/domain/user/adapter/input/mapper/AuthMapper.kt
+++ src/main/kotlin/com/gsm/notdo/domain/user/adapter/input/mapper/AuthMapper.kt
@@ -1,7 +1,9 @@
 package com.gsm.notdo.domain.user.adapter.input.mapper
 
+import com.gsm.notdo.domain.auth.apdater.input.request.SignInRequest
 import com.gsm.notdo.domain.auth.apdater.input.request.SignupRequest
 import com.gsm.notdo.domain.auth.apdater.input.response.TokenResponse
+import com.gsm.notdo.domain.auth.application.port.input.dto.SignInDto
 import com.gsm.notdo.domain.auth.application.port.input.dto.SignUpDto
 import com.gsm.notdo.domain.auth.port.output.dto.TokenDto
 
@@ -10,6 +12,11 @@ fun SignupRequest.toDto(request: SignupRequest): SignUpDto =
                 email = request.email,
                 password = request.password,
                 nickname = request.nickname
+        )
+fun SignInRequest.toDto(request: SignInRequest): SignInDto =
+        SignInDto(
+                email = request.email,
+                password = request.password
         )
 fun TokenDto.toResponse(dto: TokenDto): TokenResponse =
         TokenResponse(
