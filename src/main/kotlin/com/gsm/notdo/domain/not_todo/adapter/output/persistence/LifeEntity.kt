ea81b17482d1824a4f5e4b02acb092d14fb6298c
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-05-21 11:23:35 +0900

    :sparkles: Life persistence entity 생성

---
+++ src/main/kotlin/com/gsm/notdo/domain/not_todo/adapter/output/persistence/LifeEntity.kt
@@ -0,0 +1,21 @@
+package com.gsm.notdo.domain.not_todo.adapter.output.persistence
+
+import com.gsm.notdo.global.entity.BaseEntity
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+@Entity
+@Table(name = "tbl_life")
+data class LifeEntity(
+        override val id: UUID,
+        @Column(nullable = false)
+        val isComplete: Boolean,
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "not_to_do_history_id", nullable = false)
+        val notToDoHistoryId: NotToDoHistoryEntity
+) : BaseEntity(id)
