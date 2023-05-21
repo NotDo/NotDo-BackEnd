8a9587867c74a9030ca62d611664910eae5948ab
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-05-21 11:17:56 +0900

    :sparkles: NotToDo persistence entity 생성

--- src/main/kotlin/com/gsm/notdo/domain/not_todo/adapter/output/persistence/NotToDoEntity.kt
+++ src/main/kotlin/com/gsm/notdo/domain/not_todo/adapter/output/persistence/NotToDoEntity.kt
@@ -1,13 +1,13 @@
-package com.gsm.notdo.domain.todo.adapter.output.persistence.entity
+package com.gsm.notdo.domain.not_todo.adapter.output.persistence
 
 import com.gsm.notdo.domain.user.adapter.output.persistence.entity.UserEntity
 import com.gsm.notdo.global.entity.BaseEntity
-import java.util.*
+import java.util.UUID
 import javax.persistence.*
 
 @Entity
-@Table(name = "tbl_to_do")
-data class ToDoEntity(
+@Table(name = "tbl_not_to_do")
+data class NotToDoEntity(
         override val id: UUID,
         @Column(columnDefinition = "VARCHAR(50)", nullable = false)
         val content: String,
