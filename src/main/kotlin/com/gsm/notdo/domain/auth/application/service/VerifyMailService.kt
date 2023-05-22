c04a900a28cf827a08afb4448a2981f14b8112e4
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-05-22 10:26:10 +0900

    :sparkles: 서비스 구현

--- src/main/kotlin/com/gsm/notdo/domain/auth/application/service/VerifyMailService.kt
+++ src/main/kotlin/com/gsm/notdo/domain/auth/application/service/VerifyMailService.kt
@@ -1,9 +1,28 @@
 package com.gsm.notdo.domain.auth.application.service
 
+import com.gsm.notdo.domain.auth.application.port.input.VerifyMailUserCase
+import com.gsm.notdo.domain.auth.application.port.output.CommandAuthenticationPort
+import com.gsm.notdo.domain.auth.application.port.output.QueryAuthCodePort
+import com.gsm.notdo.domain.auth.domain.Authentication
 import org.springframework.stereotype.Service
 
 @Service
 class VerifyMailService(
+        private val queryAuthCodePort: QueryAuthCodePort,
+        private val commandAuthenticationPort: CommandAuthenticationPort
+) : VerifyMailUserCase {
+    override fun execute(email: String, authKey: String) {
+        val authCode = queryAuthCodePort.findByEmail(email)
+        if(authCode.authCode == authKey) {
+            commandAuthenticationPort.save(
+                    Authentication(
+                            email = email,
+                            attemptCount = 0,
+                            isVerified = true,
+                            expirationTime = 900
+                    )
+            )
+        }
+    }
 
-) {
 }
