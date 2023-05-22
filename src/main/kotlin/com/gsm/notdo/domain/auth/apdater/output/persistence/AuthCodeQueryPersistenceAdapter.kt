c34b9e8205fca96489396cd81823c653b83edb3f
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-05-22 10:00:08 +0900

    :sparkles: AuthCode Persistence Adapter 구현

--- src/main/kotlin/com/gsm/notdo/domain/auth/apdater/output/persistence/AuthCodeQueryPersistenceAdapter.kt
+++ src/main/kotlin/com/gsm/notdo/domain/auth/apdater/output/persistence/AuthCodeQueryPersistenceAdapter.kt
@@ -1,14 +1,18 @@
 package com.gsm.notdo.domain.auth.apdater.output.persistence
 
+import com.gsm.notdo.domain.auth.apdater.output.persistence.mapper.toDomain
 import com.gsm.notdo.domain.auth.apdater.output.persistence.repository.AuthCodeRepository
 import com.gsm.notdo.domain.auth.application.port.output.QueryAuthCodePort
 import com.gsm.notdo.domain.auth.domain.AuthCode
+import com.gsm.notdo.domain.auth.domain.exception.AuthCodeNotFoundException
 import org.springframework.stereotype.Component
 
 @Component
 class AuthCodeQueryPersistenceAdapter(
         private val authCodeRepository: AuthCodeRepository
 ) : QueryAuthCodePort {
-    override fun findByEmail(email: String): AuthCode =
-            authCodeRepository.findByEmailOrNull(email) ?:
+    override fun findByEmail(email: String): AuthCode {
+        val authCodeEntity = authCodeRepository.findByEmailOrNull(email) ?: throw AuthCodeNotFoundException()
+        return authCodeEntity.toDomain(authCodeEntity)
+    }
 }
